1d9e4e2c135fc7179fa5cdbaf40c4ed54c7b060a
NicoleLatifi <Nicole.Latifi@gmail.com>
2023-07-12 10:50:20 -0600

    add comment to realmConfig

--- realmConfig.ts
+++ realmConfig.ts
@@ -1,3 +1,7 @@
+// This module creates and exports a singleton instance of the app
+// so that the app can be accessed outside of a functional React component
+// Otherwise, the app can only be accessed with the useApp() hook within a functional React component
+
 import Realm from 'realm';
 import { APP_ID } from '@env'
 
